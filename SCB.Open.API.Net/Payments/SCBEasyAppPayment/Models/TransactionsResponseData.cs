53d134da111b1d7a1ac691f08bf72b15e8bcc332
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-29 01:20:59 +0700

    Add Transactions models

---
+++ SCB.Open.API.Net/Payments/SCBEasyAppPayment/Models/TransactionsResponseData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SCB.Open.API.Net.Payments.SCBEasyAppPayment.Models
+{
+    public class TransactionsResponseData
+    {
+    }
+}
