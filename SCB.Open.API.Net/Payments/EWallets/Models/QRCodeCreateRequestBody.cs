52a595c27caae7eaf3e4a746e10497ebb4b552c3
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-30 22:09:02 +0700

    Add Payment / eWallets QR Code Create models

---
+++ SCB.Open.API.Net/Payments/EWallets/Models/QRCodeCreateRequestBody.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SCB.Open.API.Net.Payments.EWallets.Models
+{
+    public class QRCodeCreateRequestBody
+    {
+        /// <summary>
+        /// Required:
+        /// Type of QR Code
+        /// • "A": Alipay
+        /// • "W": WeChatPay
+        /// </summary>
+        [Required]
+        public string tranType { get; set; }
+        /// <summary>
+        /// Required:
+        /// Company ID
+        /// Length: Up to 5
+        /// For sandbox: It must be number[0 - 9]
+        /// </summary>
+        [Required]
+        public string companyId { get; set; }
+        /// <summary>
+        /// Required:
+        /// Terminal ID
+        /// </summary>
+        [Required]
+        public string terminalId { get; set; }
+        /// <summary>
+        /// Required:
+        /// Unique Merchant Transaction Reference Number
+        /// Length: Up to 50
+        /// For sandbox: It must start with 'SCB'
+        /// </summary>
+        [Required]
+        public string outTradeNo { get; set; }
+        /// <summary>
+        /// Required:
+        /// Amount of transaction with the length up to 10 characters including "." e.g. 100, 100.00
+        /// </summary>
+        [Required]
+        public string totalFee { get; set; }
+    }
+}
---
+++ SCB.Open.API.Net/Payments/EWallets/Models/QRCodeCreateResponseData.cs
@@ -0,0 +1,22 @@
+﻿
+namespace SCB.Open.API.Net.Payments.EWallets.Models
+{
+    public class QRCodeCreateResponseData
+    {
+        public QRCodeCreateStatus status { get; set; }
+        public QRCodeCreateData data { get; set; }
+        public object jsonString { get; set; }
+    }
+
+    public class QRCodeCreateStatus
+    {
+        public int code { get; set; }
+        public string description { get; set; }
+    }
+
+    public class QRCodeCreateData
+    {
+        public string codeUrl { get; set; }
+        public string prepayId { get; set; }
+    }
+}
