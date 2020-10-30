e88561887f2a2f30988029fd47f73bbe850b89ef
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-30 22:31:02 +0700

    Add eWallets api url

--- SCB.Open.API.Net/Infrastructure/OpenAPI.cs
+++ SCB.Open.API.Net/Infrastructure/OpenAPI.cs
@@ -79,6 +79,67 @@ namespace SCB.Open.API.Net.Infrastructure
                     return $"{baseUri}/​v1/​payment/​merchant/​rtp/​refund";
                 }
             }
+
+            /// <summary>
+            /// Alipay or WeChatPay
+            /// </summary>
+            public class EWallets
+            {
+                /// <summary>
+                /// POST: QR Code Create
+                /// </summary>
+                /// <param name="baseUri">Base uri</param>
+                /// <returns>Route API Url</returns>
+                public static string QRCodeCreate(string baseUri)
+                {
+                    return $"{baseUri}/​v1/​payment/​ewallets/​qrcode/​create";
+                }
+                /// <summary>
+                /// POST: Inquire
+                /// </summary>
+                /// <param name="baseUri">Base uri</param>
+                /// <returns>Route API Url</returns>
+                public static string Inquire(string baseUri)
+                {
+                    return $"{baseUri}/​v1/​payment/​ewallets/​inquire";
+                }
+                /// <summary>
+                /// POST: Cancel
+                /// </summary>
+                /// <param name="baseUri">Base uri</param>
+                /// <returns>Route API Url</returns>
+                public static string Cancel(string baseUri)
+                {
+                    return $"{baseUri}/​v1/​payment/​ewallets/​cancel";
+                }
+                /// <summary>
+                /// POST: Refund
+                /// </summary>
+                /// <param name="baseUri">Base uri</param>
+                /// <returns>Route API Url</returns>
+                public static string Refund(string baseUri)
+                {
+                    return $"{baseUri}/​v1/​payment/​ewallets/​refund";
+                }
+                /// <summary>
+                /// POST: QR Code Cancel
+                /// </summary>
+                /// <param name="baseUri">Base uri</param>
+                /// <returns>Route API Url</returns>
+                public static string QRCodeCancel(string baseUri)
+                {
+                    return $"{baseUri}/​v1/​payment/​ewallets/​qrcode/​cancel";
+                }
+                /// <summary>
+                /// POST: Pay
+                /// </summary>
+                /// <param name="baseUri">Base uri</param>
+                /// <returns>Route API Url</returns>
+                public static string Pay(string baseUri)
+                {
+                    return $"{baseUri}/​v1/​payment/​ewallets/​pay";
+                }
+            }
         }
 
         public static class CustomerInfo
