212e185d14f42fe4a4f5c1540d581360062a17e0
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-11-02 15:24:11 +0700

    Add Inquire request/response models

---
+++ SCB.Open.API.Net/Payments/EWallets/Models/InquireRequestBody.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SCB.Open.API.Net.Payments.EWallets.Models
+{
+    public class InquireRequestBody
+    {
+        /// <summary>
+        /// Required:
+        /// Type of QR Code
+        /// • "A": Alipay
+        /// • "W": WeChatPay
+        /// </summary>
+        [Required]
+        public string tranType { get; set; }
+        /// <summary>
+        /// Required:
+        /// Company ID
+        /// Length: Up to 5
+        /// For sandbox: It must be number[0 - 9]
+        /// </summary>
+        [Required]
+        public string companyId { get; set; }
+        /// <summary>
+        /// Required:
+        /// Terminal ID
+        /// </summary>
+        [Required]
+        public string terminalId { get; set; }
+        /// <summary>
+        /// Required:
+        /// Unique Merchant Transaction Reference Number
+        /// Length: Up to 50
+        /// For sandbox: It must start with 'SCB'
+        /// </summary>
+        [Required]
+        public string outTradeNo { get; set; }
+    }
+}
---
+++ SCB.Open.API.Net/Payments/EWallets/Models/InquireResponseData.cs
@@ -0,0 +1,38 @@
+﻿
+namespace SCB.Open.API.Net.Payments.EWallets.Models
+{
+    public class InquireResponseData
+    {
+        public InquireStatus status { get; set; }
+        public InquireData data { get; set; }
+    }
+
+    public class InquireStatus
+    {
+        public int code { get; set; }
+        public string description { get; set; }
+    }
+
+    public class InquireData
+    {
+        public string transactionId { get; set; }
+        public string amount { get; set; }
+        public string mid { get; set; }
+        public string ref2 { get; set; }
+        public string ref1 { get; set; }
+        public string tradeState { get; set; }
+        public string nonceStr { get; set; }
+        public string tid { get; set; }
+        public string exchangeRate { get; set; }
+        public string buyerLoginId { get; set; }
+        public string timeEnd { get; set; }
+        public string cnyAmount { get; set; }
+        public string masterTid { get; set; }
+        public string ref3 { get; set; }
+        public string addressLine1 { get; set; }
+        public string addressLine2 { get; set; }
+        public object buyerUserId { get; set; }
+        public string addressLine3 { get; set; }
+        public string masterMid { get; set; }
+    }
+}
