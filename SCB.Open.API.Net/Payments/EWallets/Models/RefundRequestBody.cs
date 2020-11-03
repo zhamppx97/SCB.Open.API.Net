fbdad37a0ca11f554deaccbb9c1c52d8c014fe7d
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-11-03 15:09:09 +0700

    Add Refund request/response models

---
+++ SCB.Open.API.Net/Payments/EWallets/Models/RefundRequestBody.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SCB.Open.API.Net.Payments.EWallets.Models
+{
+    /// <summary>
+    /// Not Support Alipay
+    /// </summary>
+    public class RefundRequestBody
+    {
+        /// <summary>
+        /// Required:
+        /// Type of QR Code
+        /// • "W": WeChatPay
+        /// </summary>
+        [Required]
+        public string tranType { get; set; }
+        /// <summary>
+        /// Required:
+        /// Company ID
+        /// Length: Up to 5
+        /// For sandbox: It must be number[0 - 9]
+        /// </summary>
+        [Required]
+        public string companyId { get; set; }
+        /// <summary>
+        /// Required:
+        /// Terminal ID
+        /// </summary>
+        [Required]
+        public string terminalId { get; set; }
+        /// <summary>
+        /// Required:
+        /// Unique Merchant Transaction Reference Number
+        /// Length: Up to 50
+        /// For sandbox: It must start with 'SCB'
+        /// </summary>
+        [Required]
+        public string outTradeNo { get; set; }
+        /// <summary>
+        /// Required:
+        /// Amount of transaction with the length up to 10 characters including "." e.g. 100, 100.00
+        /// </summary>
+        [Required]
+        public string totalFee { get; set; }
+    }
+}
---
+++ SCB.Open.API.Net/Payments/EWallets/Models/RefundResponseData.cs
@@ -0,0 +1,22 @@
+﻿
+namespace SCB.Open.API.Net.Payments.EWallets.Models
+{
+    public class RefundResponseData
+    {
+        public RefundStatus status { get; set; }
+        public RefundData data { get; set; }
+        public object jsonString { get; set; }
+    }
+
+    public class RefundStatus
+    {
+        public int code { get; set; }
+        public string description { get; set; }
+    }
+
+    public class RefundData
+    {
+        public string details { get; set; }
+        public string refundId { get; set; }
+    }
+}
