c56555f3ffe615e9629cd64c2d80ea2a03b2b3c4
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-11-03 14:16:39 +0700

    Add Cancel request/response models

---
+++ SCB.Open.API.Net/Payments/EWallets/Models/CancelResponseData.cs
@@ -0,0 +1,22 @@
+﻿
+namespace SCB.Open.API.Net.Payments.EWallets.Models
+{
+    public class CancelResponseData
+    {
+        public CancelStatus status { get; set; }
+        public CancelData data { get; set; }
+        public object jsonString { get; set; }
+    }
+
+    public class CancelStatus
+    {
+        public int code { get; set; }
+        public string description { get; set; }
+    }
+
+    public class CancelData
+    {
+        public string details { get; set; }
+        public string refundId { get; set; }
+    }
+}
