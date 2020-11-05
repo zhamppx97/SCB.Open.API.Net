959aab677088002be347db3d6adb21baaa198684
Woraphon Khuarprakopkit <woraphon.k@mscsittipol.com>
2020-11-05 11:16:07 +0700

    Add QR Code cancel request/response models

---
+++ SCB.Open.API.Net/Payments/EWallets/Models/QRCodeCancelRequestBody.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SCB.Open.API.Net.Payments.EWallets.Models
+{
+    /// <summary>
+    /// Not Support Alipay
+    /// </summary>
+    public class QRCodeCancelRequestBody
+    {
+        /// <summary>
+        /// Required:
+        /// Type of QR Code
+        /// • "W": WeChatPay
+        /// </summary>
+        [Required]
+        public string tranType { get; set; }
+        /// <summary>
+        /// Required:
+        /// Company ID
+        /// Length: Up to 5
+        /// For sandbox: It must be number[0 - 9]
+        /// </summary>
+        [Required]
+        public string companyId { get; set; }
+        /// <summary>
+        /// Required:
+        /// Terminal ID
+        /// </summary>
+        [Required]
+        public string terminalId { get; set; }
+        /// <summary>
+        /// Required:
+        /// Unique Merchant Transaction Reference Number
+        /// Length: Up to 50
+        /// For sandbox: It must start with 'SCB'
+        /// </summary>
+        [Required]
+        public string outTradeNo { get; set; }
+    }
+}
---
+++ SCB.Open.API.Net/Payments/EWallets/Models/QRCodeCancelResponseData.cs
@@ -0,0 +1,15 @@
+﻿
+namespace SCB.Open.API.Net.Payments.EWallets.Models
+{
+    public class QRCodeCancelResponseData
+    {
+        public QRCodeCancelStatus status { get; set; }
+        public object jsonString { get; set; }
+    }
+
+    public class QRCodeCancelStatus
+    {
+        public int code { get; set; }
+        public string description { get; set; }
+    }
+}
