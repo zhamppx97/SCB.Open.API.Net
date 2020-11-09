9b351b428798e52afbaaabc2b6e64b629bededdd
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-11-09 14:48:43 +0700

    Add Loan Origination API URL

--- SCB.Open.API.Net/Infrastructure/OpenAPI.cs
+++ SCB.Open.API.Net/Infrastructure/OpenAPI.cs
@@ -154,5 +154,113 @@ namespace SCB.Open.API.Net.Infrastructure
                 return $"{baseUri}/​v2/​customers/​profile";
             }
         }
+
+        public static class LoanOrigination
+        {
+            /// <summary>
+            /// POST: Calculate for loan
+            /// /​v1/​loanorigination/​simulator/​calculate
+            /// </summary>
+            /// <param name="baseUri">Base uri</param>
+            /// <returns>Route API Url</returns>
+            public static string SimulatorCalculate(string baseUri)
+            {
+                return $"{baseUri}/​v1/​loanorigination/​simulator/​calculate";
+            }
+
+            /// <summary>
+            /// POST: Requests loan application initiate
+            /// /​v1/​loanorigination/​applications
+            /// </summary>
+            /// <param name="baseUri">Base uri</param>
+            /// <returns>Route API Url</returns>
+            public static string InitiateApplications(string baseUri)
+            {
+                return $"{baseUri}/​v1/​loanorigination/​applications";
+            }
+
+            /// <summary>
+            /// GET: Single application
+            /// /​v1/​loanorigination/​applications/​{applicationId}
+            /// </summary>
+            /// <param name="baseUri">Base uri</param>
+            /// <param name="applicationId">Application Id</param>
+            /// <returns>Route API Url</returns>
+            public static string SingleApplications(string baseUri, string applicationId)
+            {
+                return $"{baseUri}/​v1/​loanorigination/​applications/​{applicationId}";
+            }
+
+            /// <summary>
+            /// PUT: Updates an existing loan application
+            /// /​v1/​loanorigination/​applications/​{applicationId}
+            /// </summary>
+            /// <param name="baseUri">Base uri</param>
+            /// <param name="applicationId">Application Id</param>
+            /// <returns>Route API Url</returns>
+            public static string UpdateApplications(string baseUri, string applicationId)
+            {
+                return $"{baseUri}/​v1/​loanorigination/​applications/​{applicationId}";
+            }
+
+            /// <summary>
+            /// POST: Updates the documents attached to an existing loan application
+            /// /​v1/​loanorigination/​applications/​{applicationId}/​documents
+            /// </summary>
+            /// <param name="baseUri">Base uri</param>
+            /// <param name="applicationId">Application Id</param>
+            /// <returns>Route API Url</returns>
+            public static string UpdateDocuments(string baseUri, string applicationId)
+            {
+                return $"{baseUri}/​v1/​loanorigination/​applications/​{applicationId}/​documents";
+            }
+
+            /// <summary>
+            /// GET: The list of documents that the user uploaded and attached to his/her loan application
+            /// /​v1/​loanorigination/​applications/​{applicationId}/​documents
+            /// </summary>
+            /// <param name="baseUri">Base uri</param>
+            /// <param name="applicationId">Application Id</param>
+            /// <returns>Route API Url</returns>
+            public static string ListDocuments(string baseUri, string applicationId)
+            {
+                return $"{baseUri}/​v1/​loanorigination/​applications/​{applicationId}/​documents";
+            }
+
+            /// <summary>
+            /// DELETE: Deletes an uploaded document record
+            /// /​v1/​loanorigination/​applications/​{applicationId}/​documents/​{documentId}
+            /// </summary>
+            /// <param name="baseUri">Base uri</param>
+            /// <param name="applicationId">Application Id</param>
+            /// <param name="documentId">Document Id</param>
+            /// <returns>Route API Url</returns>
+            public static string DeleteDocuments(string baseUri, string applicationId, string documentId)
+            {
+                return $"{baseUri}/​v1/​loanorigination/​applications/​{applicationId}/​documents/​{documentId}";
+            }
+
+            /// <summary>
+            /// POST: Enables a user to submit his/her loan application
+            /// /​v1/​loanorigination/​applications/​submit
+            /// </summary>
+            /// <param name="baseUri">Base uri</param>
+            /// <returns>Route API Url</returns>
+            public static string ApplicationsSubmit(string baseUri)
+            {
+                return $"{baseUri}/​v1/​loanorigination/​applications/​submit";
+            }
+
+            /// <summary>
+            /// POST: Cancels a loan application
+            /// /​v1/​loanorigination/​applications/​cancel
+            /// </summary>
+            /// <param name="baseUri">Base uri</param>
+            /// <returns>Route API Url</returns>
+            public static string ApplicationsCancel(string baseUri)
+            {
+                return $"{baseUri}/​v1/​loanorigination/​applications/​cancel";
+            }
+        }
     }
 }
