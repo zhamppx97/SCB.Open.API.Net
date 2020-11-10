54bf335104f77297d841c9923022e11f3245bb6d
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-11-10 11:55:44 +0700

    Add initiate applications request/response models

---
+++ SCB.Open.API.Net/LoanOrigination/Models/InitiateApplicationsRequestBody.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SCB.Open.API.Net.LoanOrigination.Models
+{
+    public class InitiateApplicationsRequestBody
+    {
+        public InitiateApplicationsLoan loan { get; set; }
+    }
+
+    public class InitiateApplicationsLoan
+    {
+        /// <summary>
+        /// Required:
+        /// Product type
+        /// </summary>
+        [Required]
+        public string productIntent { get; set; }
+    }
+}
---
+++ SCB.Open.API.Net/LoanOrigination/Models/InitiateApplicationsResponseData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SCB.Open.API.Net.LoanOrigination.Models
+{
+    public class InitiateApplicationsResponseData
+    {
+        public InitiateApplicationsStatus status { get; set; }
+        public InitiateApplicationsData data { get; set; }
+        public object jsonString { get; set; }
+    }
+
+    public class InitiateApplicationsStatus
+    {
+        public int code { get; set; }
+        public string description { get; set; }
+    }
+
+    public class InitiateApplicationsData
+    {
+        public string applicationId { get; set; }
+        public DateTime applicationDateTime { get; set; }
+    }
+}
